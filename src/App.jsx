09d9f142944d00ae5b1d5dc2ccbbb0b653711392
dirage1 <dirage4635@gmail.com>
2025-06-02 15:44:04 +0900

    tailwind 필수과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-[20px]
+      py-[20px] px-[40px]">
+      {/* display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+          gap: 20px;
+          padding: 20px 40px; */}
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="flex justify-between items-center py-[10px] px-[20px]]
+    bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
+      <ul className="flex justify-center items-center gap-[20px]
+      li:list-none li:font-normal li:text-[13px]">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
@@ -10,3 +12,23 @@ export default function Header() {
     </header>
   );
 }
+/* .header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: black;
+}
+
+.header-container ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+ul li {
+  list-style: none;
+  font-weight: 400;
+  font-size: 13px;
+}*/
